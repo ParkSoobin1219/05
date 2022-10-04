443a9e28aa98bf8415b0c4a3c4aa4a4f0a08d519
ParkSoobin1219 <psb1219j@gmail.com>
2022-10-04 16:44:38 +0900

    05_03

--- main.c
+++ main.c
@@ -4,16 +4,16 @@
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
 int main(int argc, char *argv[]) {
-	int a;
-	
-	printf("정수 하나를 입력하시오:");
-	scanf("%d", &a);
-	
-	if(a>=0)
-	printf("절대값은 %d입니다.", a);
-	else if(a<0)
-	printf("절대값은 %d입니다.", -a);
-	
-	 
-	return 0;
+   int num;
+   char c;
+   
+   printf("input a string:");
+   while((c=getchar())!='\n')
+    {
+       if(c>47&&c<58)
+       num++;
+    }
+   
+   printf("the number of digits is %d", num);
+   return 0;
 }
